efe7e32ccd4e5eab1bb6e563f03347f897fad9e6
Alexandru Turcan <alexandru.turcan@codefactorygroup.com>
2018-01-23 08:33:15 +0200

    Initial commit

---
+++ Typist/Typist/Models/IndexedWord.cs
@@ -0,0 +1,11 @@
+﻿namespace Typist.Models
+{
+    public class IndexedWord
+    {
+        public string Word { get; set; }
+
+        public int Index { get; set; }
+
+        public override string ToString() => Word;
+    }
+}
