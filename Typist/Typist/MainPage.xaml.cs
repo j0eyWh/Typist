572189ff385cd9c7d06224edcff4b3690c7ad0d4
Alexandru Turcan <alexandru.turcan@codefactorygroup.com>
2018-02-01 18:07:37 +0200

    Fooling aroung

--- Typist/Typist/MainPage.xaml.cs
+++ Typist/Typist/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Text;
 using Windows.System;
 using Windows.UI;
 using Windows.UI.Xaml.Controls;
@@ -20,11 +21,16 @@ namespace Typist
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        private string _text = "text! This is a super fancy, sample text! This text is super fancy and long This is a super fancy, sample text! This text is super fancy and long";
+        //private string _text = "text! This is a super fancy, sample text! This text is super fancy and long This is a super fancy, sample text! This text is super fancy and long";
+        private string _text = "text! This is";
 
         private readonly List<IndexedWord> _words;
         private readonly List<IndexedWord> _correctWords = new List<IndexedWord>();
         private readonly List<IndexedWord> _wrongWords = new List<IndexedWord>();
+
+        private readonly List<Run> _runsDone = new List<Run>();
+        private readonly List<Run> _runsCurrent = new List<Run>();
+        private List<Run> _runsLeft = new List<Run>();
 
         private readonly Paragraph _paragraph;
 
@@ -46,7 +52,9 @@ namespace Typist
 
             _paragraph = new Paragraph();
 
-            foreach (var run in GenerateRuns(string.Empty))
+            GenerateInitialRuns();
+
+            foreach (var run in _runsDone.Concat(_runsCurrent).Concat(_runsLeft))
             {
                 _paragraph.Inlines.Add(run);
             }
@@ -54,31 +62,68 @@ namespace Typist
             TextBlock.Blocks.Add(_paragraph);
         }
 
+        private void GenerateInitialRuns()
+        {
+            _runsDone.Clear();
+
+            _runsCurrent.Add(new Run()
+            {
+                Text = $"{_words.ElementAt(_wordIndex).Word} "
+            });
+
+            _runsLeft = _words.Skip(_wordIndex + 1).Select(x => new Run()
+            {
+                Text = $"{x.Word} "
+            }).ToList();
+        }
+
         private void HandleKeyUp(object sender, KeyRoutedEventArgs e)
         {
-            var text = InputTextBox.Text;
+            var currentWord = _words.ElementAt(_wordIndex);
 
-            if (e.Key != VirtualKey.Space)
+            var input = InputTextBox.Text;
+
+            _runsCurrent.Clear();
+
+            if (currentWord.Word.StartsWith(input))
             {
-                RerenderParagraph(text);
-                return;
-            };
+                string x;
 
+                if (input.Length == 1)
+                {
+                    x = currentWord.Word.Split(new char[] { input.First() }, 2)
+                        .LastOrDefault();
+                }
+                else
+                {
+                    x = currentWord.Word.Split(new[] { input }, StringSplitOptions.RemoveEmptyEntries).FirstOrDefault();
+                }
 
-            if (string.IsNullOrWhiteSpace(text))
-                return;
+                _runsCurrent.Add(new Run()
+                {
+                    Text = input,
+                    Foreground = new SolidColorBrush(Colors.ForestGreen)
+                });
+                if (x != null)
+                {
+                    _runsCurrent.Add(new Run()
+                    {
+                        Text = x,
+                    });
+                }
+            }
 
-            var word = _words.ElementAt(_wordIndex);
+            Redraw();
+        }
 
-            if (text.TrimEnd() == word.Word)
-                _correctWords.Add(word);
-            else
-                _wrongWords.Add(word);
+        private void Redraw()
+        {
+            _paragraph.Inlines.Clear();
 
-            _nicePrerun = string.Empty;
-            this.InputTextBox.Text = string.Empty;
-            _wordIndex++;
-            RerenderParagraph(text);
+            foreach (var run in _runsDone.Concat(_runsCurrent.Concat(_runsLeft)))
+            {
+                _paragraph.Inlines.Add(run);
+            }
         }
 
         private void RerenderParagraph(string input)
